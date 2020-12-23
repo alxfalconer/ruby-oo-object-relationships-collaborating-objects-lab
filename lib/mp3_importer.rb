acead3b667c9a56805058750999f5afcab514a7f
Alexander <al.x.falconer@gmail.com>
2020-12-23 18:23:06 -0500

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+  attr_accessor :path
+  def initialize(path)
+    # sets file path
+    @path = path
+  end
+
+  def files
+    # returns array of all filenames in path
+    Dir.children(@path)
+  end
+
+  def import
+    # Song.new_by_filename each file in files
+    files.each { |file| Song.new_by_filename(file) }
+  end
+end
